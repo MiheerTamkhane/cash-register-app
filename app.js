2c4c9f425ebe64c7772fe4f7d54cc55bdfc76d73
Miheer Vilas Tamkhane <sarangtamkhane10@gmail.com>
2021-08-03 14:43:18 +0530

    final commit

--- app.js
+++ app.js
@@ -5,23 +5,27 @@ const errMsgDiv = document.querySelector("#messageDiv");
 const numberOfNotes = document.querySelectorAll(".noOfNotes");
 const nextBtn = document.querySelector("#next-btn");
 const cashGivenDiv = document.querySelector("#cash-given");
+const table = document.querySelector("#table");
 const notesArr = [2000, 500, 200, 100, 50, 20, 10, 5, 1];
 
 //Click Event function
 nextBtn.addEventListener("click", () => {
   hideErrMsg();
-  if (Number(billAmt.value)) {
-    cashGivenDiv.style.display = "block";
+  if (Number(billAmt.value) > 0) {
+    cashGivenDiv.style.display = "flex";
+    nextBtn.style.display = "none";
   } else {
-    errMsgHandler("Please enter bill amount");
+    errMsgHandler("Please enter valid bill amount");
   }
 });
 button.addEventListener("click", () => {
   hideErrMsg();
+
   if (billAmt.value > 0) {
     if (cashAmt.value >= billAmt.value) {
       const returnAmt = cashAmt.value - billAmt.value;
       calculateAmt(returnAmt);
+      table.style.display = "block";
       if (returnAmt == 0) {
         errMsgHandler("No return amount to give!");
       }
@@ -42,6 +46,11 @@ function calculateAmt(returnAmt) {
     const notes = Math.trunc(returnAmt / notesArr[i]);
     returnAmt = returnAmt % notesArr[i];
     numberOfNotes[i].innerText = notes;
+
+    if (notes > 0) {
+      numberOfNotes[i].style.backgroundColor = "#54E346";
+      numberOfNotes[i].style.color = "black";
+    }
   }
 }
 
